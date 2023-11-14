2313ef408f7d1d30426a63196e711e463e4906df
ncng <nnct2000@gmail.com>
2023-11-14 17:54:39 +0700

    Add links to titles

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,6 +41,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          showLastUpdateTime: true,
         },
         blog: false, // Disable blog
         // {
