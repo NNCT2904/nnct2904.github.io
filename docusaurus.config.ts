b8be76ed23dc3d24ee4545e52e96d9ab98d4c0f8
ncng <nnct2000@gmail.com>
2024-01-02 23:01:28 +0700

    Upgrade docusaurus to 3.0.1, add social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,43 +1,43 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: 'Thanh Nguyen Home',
-  tagline: 'Dinosaurs-based resume page',
-  favicon: 'img/icon.png',
+  title: "Thanh Nguyen Home",
+  tagline: "Dinosaurs-based resume page",
+  favicon: "img/icon.png",
 
   // Set the production url of your site here
-  url: 'https://nnct2904.github.io',
+  url: "https://nnct2904.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'nnct2904', // Usually your GitHub org/user name.
-  projectName: 'nnct2904.github.io', // Usually your repo name.
-  deploymentBranch:'gh-page',
+  organizationName: "nnct2904", // Usually your GitHub org/user name.
+  projectName: "nnct2904.github.io", // Usually your repo name.
+  deploymentBranch: "gh-page",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          routeBasePath: '/', // Serve the docs at the site's root
+          routeBasePath: "/", // Serve the docs at the site's root
           /* other docs plugin options */
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
@@ -52,7 +52,7 @@ const config: Config = {
         //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         // },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -60,29 +60,29 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/social-card.jpg",
     navbar: {
-      title: 'Thanh Nguyen',
+      title: "Thanh Nguyen",
       // logo: {
       //   alt: 'Thanh Nguyen Logo',
       //   src: 'img/icon.png',
       // },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'docSidebar',
-          position: 'left',
-          label: 'Portfolio',
+          type: "docSidebar",
+          sidebarId: "docSidebar",
+          position: "left",
+          label: "Portfolio",
         },
         // {
-        //   to: '/blog', 
-        //   label: 'Blog', 
+        //   to: '/blog',
+        //   label: 'Blog',
         //   position: 'left'
         // },
         {
-          href: 'https://github.com/NNCT2904',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/NNCT2904",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
